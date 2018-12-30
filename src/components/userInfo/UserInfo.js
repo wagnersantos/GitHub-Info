cb5e48d264956f5c149731991b7faac11e56a570
wagnersantos <wagtvc@gmail.com>
2018-12-30 15:29:18 -0200

    adicionando componente UserInfo

---
+++ src/components/userInfo/UserInfo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const UserInfo = {user} => (
+	user ?
+		(
+			<div className='row'>
+				<div className='col'>
+					<img  className='img-circle' src={user.avatar_url} alt='avatar' width='140' 
+						height='140'/>
+					<h2>{user.login}</h2>
+					<p>{user.name}</p>
+					<p>Followers: {user.followers} / Following: {user.following}</p>
+					<p><a className='btn btn-default' href={user.html_url} role ='button'>View details</a></p>
+				</div>
+			</div>
+		) : null
+);
+
+export default UserInfo; 
