e6c502a80f928be469ac9a18b900d6a03f1d77aa
wagnersantos <wagtvc@gmail.com>
2018-12-30 13:03:53 -0200

    adicionando método updateRepos no componente GitHub

--- src/components/gitHub/GitHub.js
+++ src/components/gitHub/GitHub.js
@@ -9,14 +9,18 @@ class GitHub extends Component{
 			repos: [],
 		}
 		this.updateUser = this.updateUser.bind(this); 
+		this.updateRepos = this.updateRepos.bind(this);
 	}
 	updateUser(user){
 		this.setState({user: user});
 	}
+	updateRepos(repos){
+		this.setState({repos: repos});
+	}
 	render(){
 		return (
 			<div className="container">
-       			<Search updateUser={this.updateUser} />
+       			<Search updateUser={this.updateUser} updateRepos={this.updateRepos} />
       		</div>
 		)
 	}
