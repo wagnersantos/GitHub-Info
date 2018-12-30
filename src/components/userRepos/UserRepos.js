c13cbb5474e0c421e7dc863ed55d25091621def6
wagnersantos <wagtvc@gmail.com>
2018-12-30 16:02:30 -0200

    adicionando método componentWillReceiveProps e setando estado de contagem de repositorio

--- src/components/userRepos/UserRepos.js
+++ src/components/userRepos/UserRepos.js
@@ -5,30 +5,34 @@ class UserRepos extends Component{
 	    super(props);
 	    this.state = {count: null};
 	}
+	componentWillReceiveProps(props){
+		this.setState({count: props.repos.length});
+	}
 	render(){
-		return(
-			<div>
-				{
-					this.props.repos.map( (repo,index) =>
-						(
-							<div className='thumbnail' key={index}>
-								<div className='caption'>
-									<h3>{repo.name}
-										<span className='badge'>{repo.stargazers_count} STARS</span>
-									</h3>
-									<p>{repo.description}</p>
-									<p>
-										<a href={repo.html_url} className='btn btn-primary' role='button'>Repository</a>
-										<a href={`${repo.html_url}/issues`} className='btn btn-default' role='button'>
-										Issues ({repo.open_issues})</a>
-									</p>
-								</div>
-							</div>
-						)
-					)
-				}
-			</div>
-		)
+			const repos = this.props.repos.map( (repo,index) =>
+				(
+					<div className='thumbnail' key={index}>
+						<div className='caption'>
+							<h3>{repo.name}
+								<span className='badge'>{repo.stargazers_count} STARS</span>
+							</h3>
+							<p>{repo.description}</p>
+							<p>
+								<a href={repo.html_url} className='btn btn-primary' role='button'>Repository</a>
+								<a href={`${repo.html_url}/issues`} className='btn btn-default' role='button'>
+								Issues ({repo.open_issues})</a>
+							</p>
+						</div>
+					</div>
+				)
+			)
+		
+			return(
+				<div>
+					<h2>{this.state.count} Repositories</h2>
+					{repos}
+				</div>
+			)
 	}
 }
 
