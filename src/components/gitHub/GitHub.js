2a42623c5c594dc30bf66f69200364c10ff41272
wagnersantos <wagtvc@gmail.com>
2018-12-30 12:58:31 -0200

    adicionando construtor e setando estados no componenete GitHub

--- src/components/gitHub/GitHub.js
+++ src/components/gitHub/GitHub.js
@@ -2,6 +2,13 @@ import React, {Component} from 'react';
 import Search from '../search/Search';
 
 class GitHub extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			user: null,
+			repos: [],
+		}
+	}
 	render(){
 		return (
 			<div className="container">
