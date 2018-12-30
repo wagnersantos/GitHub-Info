f8c5f6f8cd4df2c1ac488a04cd48c7bb45630062
wagnersantos <wagtvc@gmail.com>
2018-12-30 15:46:05 -0200

    adicionando componente UserProfile e chamando-o no componente UserInfo

--- src/components/userInfo/UserInfo.js
+++ src/components/userInfo/UserInfo.js
@@ -1,17 +1,13 @@
 import React from 'react';
+import UserProfile from '../userProfile/UserProfile';
 
 const UserInfo = ({user}) => (
 	user ?
 		(
 			<div className='row'>
 				<div className='col'>
-					<img  className='img-circle' src={user.avatar_url} alt='avatar' width='140' 
-						height='140'/>
-					<h2>{user.login}</h2>
-					<p>{user.name}</p>
-					<p>Followers: {user.followers} / Following: {user.following}</p>
-					<p><a className='btn btn-default' href={user.html_url} role ='button'>View details</a></p>
-				</div>
+					<UserProfile user={user}/>
+				</div>	
 			</div>
 		) : null
 );
---
+++ src/components/userProfile/UserProfile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UserProfile = ({user}) => (
+	<div>
+		<img  className='img-circle' src={user.avatar_url} alt='avatar' width='140' 
+			height='140'/>
+		<h2>{user.login}</h2>
+		<p>{user.name}</p>
+		<p>Followers: {user.followers} / Following: {user.following}</p>
+		<p><a className='btn btn-default' href={user.html_url} role ='button'>View details</a></p>
+	</div>
+)
+
+export default UserProfile;
